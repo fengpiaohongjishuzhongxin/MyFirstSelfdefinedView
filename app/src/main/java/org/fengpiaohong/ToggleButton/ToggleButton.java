560a659ba9e0bd4df02f6608d40e4991713518d5
515973568@qq.com <515973568@qq.com>
2017-03-05 17:10:18 +0800

    Initial commit

---
+++ app/src/main/java/org/fengpiaohong/ToggleButton/ToggleButton.java
@@ -0,0 +1,31 @@
+package org.fengpiaohong.ToggleButton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View;
+
+/**
+ * Created by 乘耀 on 2017/3/5.
+ */
+
+public class ToggleButton extends View implements View.OnClickListener {
+    public ToggleButton(Context context) {
+        this(context,null);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
